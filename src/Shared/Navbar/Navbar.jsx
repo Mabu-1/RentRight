6af1af72b02cc451d8c3f09f4f41f0a28a6e1b7f
Mabu-1 <umahtab65@gmail.com>
2025-10-05 09:02:15 +0600

    hamburger mneu fixed

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -32,20 +32,20 @@ const Navbar = () => {
             onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
             className="p-2 rounded-md hover:bg-gray-200 transition"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="black"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d={isMobileMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
-              />
-            </svg>
+          <svg
+  xmlns="http://www.w3.org/2000/svg"
+  className="h-6 w-6 svg-text"
+  fill="none"
+  viewBox="0 0 24 24"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    d={isMobileMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+  />
+</svg>
+
           </button>
         </div>
 
